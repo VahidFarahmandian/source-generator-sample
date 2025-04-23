46c65da3466beaf135cccf709abfad4d3bfc30b3
Vahid Farahmandian <farahmandian2011@gmail.com>
2025-04-23 17:47:10 +0330

    .vs ignored

--- SourceGenerators.WebAPI/Program.cs
+++ SourceGenerators.WebAPI/Program.cs
@@ -1,15 +1,5 @@
-using SourceGenerators.WebAPI;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
-app.MapOpenApi();
-
-app.MapGet("/add/{a:int}/{b:int}", (int a, int b) => new Calculator().Add(a, b));
-app.MapGet("/subtract/{a:int}/{b:int}", (int a, int b) => new Calculator().Subtract(a, b));
-app.MapGet("/multiply/{a:int}/{b:int}", (int a, int b) => new Calculator().Multiply(a, b));
-app.MapGet("/divide/{a:int}/{b:int}", (int a, int b) => new Calculator().Divide(a, b));
 
 app.Run();
